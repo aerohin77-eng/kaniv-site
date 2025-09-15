e837fd0ce6d48a8f7e2f2cb37c97b340ce9c1314
aerohin77-eng <a.erohin77@gmail.com>
2025-09-15 12:45:00 +0300

    fix: correct globals.css path

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,11 @@
-export const metadata = {
-  title: "Центр краси та здоров'я · Канів",
-  description: "Косметика, аналізи, косметологія та масаж. Онлайн-замовлення косметики та запис на прийом.",
-};
+{\rtf1\ansi\ansicpg1252\paperw11900\paperh16840\margl567\margr567\margt567\margb567\vieww18540\viewh17280\viewscale100\lin0\rin0
+{\papercolor16777215}
+{\fonttbl\f0\fnil\fcharset0 Helvetica;}
+{\colortbl\red0\green0\blue0;\red255\green255\blue255;}
+\pard
+{\*\pageHeader Helvetica::12px::right::black::0.5::NO::Header Author: Zoe Appleseed [date] - Page [pagenumber] of [totalpages]}
+{\*\pageFooter Helvetica::12px::right::black::0.5::NO::Footer Author: Zoe Appleseed [title] - Page [pagenumber] of [totalpages]}
+{\*\background {\shp{\*\shpinst\shpleft0\shptop0\shpright0\shpbottom0\shpfhdr0\shpbxmargin\shpbymargin\shpwr0\shpwrk0\shpfblwtxt1\shpz0\shplid1025{\sp{\sn shapeType}{\sv 1}}{\sp{\sn fFlipH}{\sv 0}}{\sp{\sn fFlipV}{\sv 0}}{\sp{\sn fillColor}{\sv 16777215}}{\sp{\sn fFilled}{\sv 1}}{\sp{\sn lineWidth}{\sv 0}}{\sp{\sn fLine}{\sv 0}}{\sp{\sn bWMode}{\sv 9}}{\sp{\sn fBackground}{\sv 1}}}}}
 
-import "./globals.css";
-
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="uk">
-      <body>{children}</body>
-    </html>
-  );
+\f0\fs24 \cf0 export const metadata =   title: "\u1062?\u1077?\u1085?\u1090?\u1088? \u1082?\u1088?\u1072?\u1089?\u1080? \u1090?\u1072? \u1079?\u1076?\u1086?\u1088?\u1086?\u1074?'\u1103? \'b7 \u1050?\u1072?\u1085?\u1110?\u1074?",  description: "\u1050?\u1086?\u1089?\u1084?\u1077?\u1090?\u1080?\u1082?\u1072?, \u1072?\u1085?\u1072?\u1083?\u1110?\u1079?\u1080?, \u1082?\u1086?\u1089?\u1084?\u1077?\u1090?\u1086?\u1083?\u1086?\u1075?\u1110?\u1103? \u1090?\u1072? \u1084?\u1072?\u1089?\u1072?\u1078?. \u1054?\u1085?\u1083?\u1072?\u1081?\u1085?-\u1079?\u1072?\u1084?\u1086?\u1074?\u1083?\u1077?\u1085?\u1085?\u1103? \u1082?\u1086?\u1089?\u1084?\u1077?\u1090?\u1080?\u1082?\u1080? \u1090?\u1072? \u1079?\u1072?\u1087?\u1080?\u1089? \u1085?\u1072? \u1087?\u1088?\u1080?\u1081?\u1086?\u1084?.",;import "../styles/globals.css";export default function RootLayout( children :  children: React.ReactNode )   return (    <html lang="uk">      <body>children</body>    </html>  );\
 }
